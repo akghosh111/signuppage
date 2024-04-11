6d48591133cf97bcad7cc10d9bbcb1a001f5dd6c
akghosh111 <ghoshkiran2017@gmail.com>
2024-04-11 18:27:15 +0530

    added password validation & create account button

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+
+    let error = document.querySelector("error");
+
+    if (password == "" && confirmPassword == "") {
+        document.getElementById('error').innerHTML = "*passwords do not match";
+    }
+
+    else if (password == confirmPassword) {
+        document.getElementById('error').innerHTML = "";
+    }
+
+
+
+
     console.log("Hi");
 })
